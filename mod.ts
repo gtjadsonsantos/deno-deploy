cfa7cfc127982296336c8556c1ddc0d18f913530
Jadson Santos <42282908+jadson179@users.noreply.github.com>
2021-06-26 12:44:45 -0300

    Update mod.ts

--- mod.ts
+++ mod.ts
@@ -1,11 +1,6 @@
-import { h } from "https://x.lcas.dev/preact@10.5.12/mod.js";
-
 addEventListener("fetch", (event) => {
-    
-  console.log(event)
-  const response = new Response("hellow smarthome"), {
-    headers: { "content-type": "text/html; charset=utf-8" },
+  const response = new Response("Hello World!", {
+    headers: { "content-type": "text/plain" },
   });
-
   event.respondWith(response);
 });
