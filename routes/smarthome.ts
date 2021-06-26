425f653d1b448980b76db3020e2947a1f6f4a61f
Jadson Santos <jadson44.santos@gmail.com>
2021-06-26 13:20:06 -0300

    update

--- routes/smarthome.ts
+++ routes/smarthome.ts
@@ -1,12 +1,19 @@
 import { Router } from "https://deno.land/x/oak/mod.ts";
 
+import { proxy } from "https://deno.land/x/oak_http_proxy@1.3.0/mod.ts";
+
+
 const router  = new Router()
 
-router.get("/:id",({request,response}) =>{
 
-    response.body = "Jadson dos Santos Silva"
+router.get("/:id",proxy("https://becocastelo.unicontrol.me",{
 
-})
+    srcResDecorator: (req, res, proxyRes, proxyResData) => {
+        res = proxyRes
+        res.body = proxyResData
+    }
+
+}))
 
 
 
