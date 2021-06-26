3f420895caed558c5a83515713bf3a97f177eda4
Jadson Santos <42282908+jadson179@users.noreply.github.com>
2021-06-26 12:43:41 -0300

    Update mod.ts

--- mod.ts
+++ mod.ts
@@ -1,23 +1,9 @@
 import { h } from "https://x.lcas.dev/preact@10.5.12/mod.js";
-import { renderToString } from "https://x.lcas.dev/preact@10.5.12/ssr.js";
-
-function App() {
-  return (
-    <html>
-      <head>
-        <title>Hello from JSX</title>
-      </head>
-      <body>
-        <h1>Hello world</h1>
-      </body>
-    </html>
-  );
-}
 
 addEventListener("fetch", (event) => {
     
   console.log(event)
-  const response = new Response(renderToString(<App />), {
+  const response = new Response("hellow smarthome"), {
     headers: { "content-type": "text/html; charset=utf-8" },
   });
 
